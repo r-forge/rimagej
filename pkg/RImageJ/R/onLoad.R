94829c6ef548e057fff27681cc9956c3aa8d0b87
phgrosjean (Prof. ) <phgrosjean@sciviews.org>
2010-04-12 07:38:24 +0000

    Trying to solve the threading problem on the Mac (but not 100% OK yet)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rimagej@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RImageJ/R/onLoad.R
+++ pkg/RImageJ/R/onLoad.R
@@ -1,4 +1,10 @@
-.onLoad <- function(libname, pkgname) {
-  .jpackage(pkgname)
-  cat( "ImageJ version : ", IJ$getVersion(), "\n" )
+.onLoad <- function (libname, pkgname) {
+	if (substring(R.Version()$os, 1, 6) == "darwin") {
+		# There is a problem starting Java with AWT on Mac OS X (Snow) Leopard
+		# This tries to correct it!
+		options(java.parameters = paste(getOption("java.parameters"),
+			"-Djava.awt.headless=true"))
+	}
+	.jpackage(pkgname)
+	cat("ImageJ version: ", IJ$getVersion(), "\n", sep = "")
 }
