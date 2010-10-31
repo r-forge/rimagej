f20d30f1342d5afc35d505ad3f2acdfe69f304ac
phgrosjean (Prof. ) <phgrosjean@sciviews.org>
2010-10-31 17:03:49 +0000

    Added an ImageJ object that instanciates ImageJ's menus and toolbar (full access of all ImageJ's functions from within R)!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rimagej@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RImageJ/R/onLoad.R
+++ pkg/RImageJ/R/onLoad.R
@@ -9,5 +9,11 @@
 	#       rely on genericDialog still do not work. The only solution I [PhG]
 	#       have found until now is to start RImageJ from within JGR.
 	.jpackage(pkgname)
+	if (!is.null(ImageJ)) ImageJ$show()
 	packageStartupMessage("ImageJ version: ", IJ$getVersion(), "\n", sep = "")
+}
+
+.onUnload <- function (libpath) {
+	## Close ImageJ
+	try(ImageJ$quit(), silent = TRUE)	
 }
